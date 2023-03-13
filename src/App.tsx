9b75bd525dd8b4af0080028a0862ffdd158d5e76
Thejas Chandrashekar <thejas.c@ceegees.in>
2023-03-13 14:57:14 +0530

    Submit button added

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import './App.css';
 import Calendar from './component/Calendar';
 
 function App() {
-  const [presentDate, setPresentDate] = useState('')
+  const [presentDate, setPresentDate] = useState('2023-3-12')
+  const [temp, setTemp] = useState('')
   return (
     <div className="App">
       <input type="date"
-      value={presentDate}
+      value={temp}
       onChange={(e) => {
-        setPresentDate(e.target.value)
+        setTemp(e.target.value)
       }} />
+      <button 
+      onClick={() => setPresentDate(temp)}
+      disabled={temp?.length < 1}
+      >Submit</button>
      <Calendar date={new Date(presentDate)} />
     </div>
   );
